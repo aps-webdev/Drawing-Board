61b8410a80afc38bb4ea982574ffa196ba5c406b
aps-webdev <amanprataps31@gmail.com>
2024-01-03 00:18:05 +0530

    changing the server url

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const SOCKET_SERVER =
   process.env.NODE_ENV === "production"
-    ? "https://drawing-board-server-ydb1.onrender.com"
+    ? "https://drawing-board-server-74vr.onrender.com"
     : "http://localhost:5000";
 
 export const socket = io(SOCKET_SERVER);
